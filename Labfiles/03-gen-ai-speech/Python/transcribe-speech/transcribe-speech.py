ed5116b9bcb1cf777c6687c0758ac7599ed89387
BenKalender <atayanil01@gmail.com>
2026-08-10 18:45:58 +0300

    Lab 3 speech to text

--- Labfiles/03-gen-ai-speech/Python/transcribe-speech/transcribe-speech.py
+++ Labfiles/03-gen-ai-speech/Python/transcribe-speech/transcribe-speech.py
@@ -4,7 +4,8 @@ from playsound3 import playsound
 from dotenv import load_dotenv
 
 # Import namespaces
-
+from openai import AzureOpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 
 
 def main():
@@ -13,22 +14,35 @@ def main():
         os.system('cls' if os.name == 'nt' else 'clear')
         
         # Get Configuration Settings
-        load_dotenv()
-        endpoint = os.getenv("MODEL_ENDPOINT")
-        model_deployment = os.getenv("MODEL_NAME")
+        load_dotenv(dotenv_path=Path(__file__).parent.parent.parent.parent.parent / ".env")        
+        endpoint = os.getenv("STT_MODEL_ENDPOINT")
+        model_deployment = os.getenv("STT_MODEL_NAME")
         file_path = Path(__file__).parent / "speech.wav"
         
         # Play the speech file
         playsound(file_path)
         
         # Create the Azure OpenAI client
+        token_provider = get_bearer_token_provider(                    
+            DefaultAzureCredential(), "https://ai.azure.com/.default"
+        )
 
-
+        client = AzureOpenAI(
+            azure_endpoint=endpoint,
+            azure_ad_token_provider = token_provider,
+            api_version="2025-03-01-preview"
+        )
         
         # Call model to transcribe audio file
-
-
-
+        audio_file = open(file_path, "rb")
+        transcription = client.audio.transcriptions.create(
+            model=model_deployment,
+            file=audio_file,
+            response_format="text"
+        )
+            
+        print(transcription)
+    
 
     except Exception as ex:
         print(ex)
